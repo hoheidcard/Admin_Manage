bcd103b2885a25f8b7436ca21e2696bd682d9357
ankitjain0748 <ankitjain0748@gmail.com>
2025-05-17 08:48:22 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     historyApiFallback: true
+  },
+  build: {
+    outDir: "dist"
   }
 });
